4cf148ba3c098ffab4e68130b986e001f4962fc5
MGamezG <l171130314@gamadero.tecnm.mx>
2021-10-27 19:28:00 -0500

    rutas multiples

--- server/controllers/homeControllers.js
+++ server/controllers/homeControllers.js
@@ -1,12 +1,17 @@
 /* eslint-disable prettier/prettier */
 const index = (req, res) => {
-  res.render('index', { title: 'ProyectNotes' });
+  res.render('home/index', { title: 'ProyectNotes' });
 };
 const greeting =(req, res)=>{
     // res.send('hola campeon de la Fullstack web')
       res.status(200).json({ message: 'hola campeon de la Fullstack web' });
     }
+const about = (req, res) => {
+  res.render('home/about',{title:'acerca de projnotes'})
+}
 export default {
   index,
   greeting,
+  about,
+
 };
--- server/routes/home.js
+++ server/routes/home.js
@@ -6,7 +6,9 @@ import homeController from '@server/controllers/homeControllers';
 // creando la instancia de un controlador
 const router = new Router();
 // get'/'
-router.get('/', homeController.index);
+router.get(['/','/index'], homeController.index);
+// get'/about'
+router.get('/about', homeController.about);
 // get'/ greeting'
 router.get('/greeting', homeController.greeting);
 // exportando el router que maneja las subrutas para home
