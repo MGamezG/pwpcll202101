e40d325352fb73b97d1d4d7c71752f72cb4538cb
MGamezG <l171130314@gamadero.tecnm.mx>
2021-11-04 11:00:13 -0600

    colores con materialize

--- server/controllers/homeControllers.js
+++ server/controllers/homeControllers.js
@@ -7,7 +7,7 @@ const greeting =(req, res)=>{
       res.status(200).json({ message: 'hola campeon de la Fullstack web' });
     }
 const about = (req, res) => {
-  res.render('home/about',{title:'acerca de projnotes'})
+  res.render('home/about',{appVersion:'0.0.1'});
 }
 export default {
   index,
